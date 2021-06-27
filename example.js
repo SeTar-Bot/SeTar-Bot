4f43ba8bfc8594f4afbc4bc2060c639474fbea58
EhsanFox <EhsanFox@users.noreply.github.com>
2021-06-27 14:35:55 +0430

    Added Examples

--- example.js
+++ example.js
@@ -1,1 +1,25 @@
+const Logger = require("@setar/logger");
+const { Client, WebhookClient } = require("discord.js");
 
+const myClient = new Client();
+const myWebhookClient = new WebhookClient('WEBHOOK_ID', 'WEBHOOK_TOKEN');
+
+const MyConsole = new Logger({
+ sentry: 'http://.................',
+ Log: {
+  Connection: myClient, 
+  ConnectionData: '12345.....' // CHANNEL ID
+ },
+ Error: {
+  Connection: myWebhookClient
+ }
+});
+
+// Test Log?
+MyConsole.log('this an example.')
+
+// 1 Time Error Log with Custom Sentry?
+const oneTimeError = {
+  sentry: 'http://.........'
+};
+MyConsole.error(new Error('this an example', oneTimeError);
